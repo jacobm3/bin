66c65e633f5cb9e01b411e1822911a6fb36db8e5
Jacob Martinson <jacobm3@gmail.com>
2022-09-07 15:35:12 -0700

    Update k3s init scripts.

--- start-k3s.sh
+++ start-k3s.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 sudo screen -d -m -L -Logfile /var/log/k3s.log /usr/local/bin/k3s server
+sleep 5
+sudo chown root:ubuntu /etc/rancher/k3s/k3s.yaml
+sudo chmod 640 /etc/rancher/k3s/k3s.yaml
 
--- stop-k3s.sh
+++ stop-k3s.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/bash -x
 
-sudo pkill k3s
+sudo screen -r
 ps aux | grep /var/lib/rancher/k3s | grep -v grep | awk '{print $2}' | sudo xargs kill
 sudo /usr/local/bin/k3s-killall.sh
 
