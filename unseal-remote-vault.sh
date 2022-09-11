68aa5320a9612cada5c8f62d6e069259abb0a79e
Jacob <jacobm3@gmail.com>
2022-09-11 15:26:57 -0500

    Update unseal-remote-vault.sh

--- unseal-remote-vault.sh
+++ unseal-remote-vault.sh
@@ -8,5 +8,5 @@
 VAULT_ADDR=https://vault.cosmic-security.net:8200
 
 vault status >/dev/null 2>&1 || \
-curl -s --request POST --data @.unseal.json \
+curl -s --request POST --data @bin/.unseal.json \
   ${VAULT_ADDR}/v1/sys/unseal >/dev/null
