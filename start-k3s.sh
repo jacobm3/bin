c268498f44c6a0dec9b5cf55a1e06942446afcc9
Jacob <jacobm3@gmail.com>
2022-10-16 21:00:37 -0500

    Update start-k3s.sh

--- start-k3s.sh
+++ start-k3s.sh
@@ -2,8 +2,8 @@
 
 sudo screen -d -m -L -Logfile /var/log/k3s.log /usr/local/bin/k3s server
 sleep 5
-sudo chown root:ubuntu /etc/rancher/k3s/k3s.yaml
-sudo chmod 640 /etc/rancher/k3s/k3s.yaml
+sudo chown ubuntu:ubuntu /etc/rancher/k3s/k3s.yaml
+sudo chmod 600 /etc/rancher/k3s/k3s.yaml
 
 set -x
 kubectl cluster-info
