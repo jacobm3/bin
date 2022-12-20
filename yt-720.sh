6758eb302f422d64dc41df6ec9342853a648375c
Jacob Martinson <jacobm3@gmail.com>
2022-12-20 16:55:27 -0600

    add playlist id in path

--- yt-720.sh
+++ yt-720.sh
@@ -10,5 +10,5 @@ fi
 yt-dlp -f "bestvideo[height<=720]+bestaudio/best[height<=720]" \
  --yes-playlist \
  --embed-chapters \
- -o "%(channel)s/%(playlist_title)s/%(playlist_index)s-%(title)s-%(id)s.%(ext)s" \
+ -o "%(channel)s/%(playlist_title)s-%(playlist_id)s/%(playlist_index)s-%(title)s-%(id)s.%(ext)s" \
   https://www.youtube.com/playlist?list=$PLID
