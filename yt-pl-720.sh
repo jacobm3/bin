7432f51f5170e12b16239ef3e8d4b0d7cf53a443
Jacob Martinson <jacobm3@gmail.com>
2023-02-17 21:35:38 -0600

    yt dlp update

--- yt-pl-720.sh
+++ yt-pl-720.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if (( $# < 1 )); then
-  echo "$0 URL"
+  echo "$0 PLAYLIST_ID"
   exit 1
 else
   PLID="$1"; shift
@@ -11,4 +11,4 @@ yt-dlp -f "bestvideo[height<=720]+bestaudio/best[height<=720]" \
  --yes-playlist \
  --embed-chapters \
  -o "%(channel)s/%(playlist_title)s-%(playlist_id)s/%(playlist_index)s-%(title)s-%(id)s.%(ext)s" \
-  $PLID
+  https://www.youtube.com/playlist?list=$PLID
