66d1104e027d5fbfe402bef3ff3a05ecd1a36435
Jacob Martinson <jacobm3@gmail.com>
2023-02-20 12:10:05 -0600

    automate k3s wsl2 init rc scripts better.

--- start-k3s.sh
+++ start-k3s.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo screen -d -m -L -Logfile /var/log/k3s.log /usr/local/bin/k3s server
+sudo screen -d -m -L -Logfile /var/log/k3s.log -S k3s-server /usr/local/bin/k3s server
 sleep 5
 sudo chown ubuntu:ubuntu /etc/rancher/k3s/k3s.yaml
 sudo chmod 600 /etc/rancher/k3s/k3s.yaml
--- stop-k3s.sh
+++ stop-k3s.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 
-sudo screen -r
+#sudo screen -r
+sudo screen -XS k3s-server quit
 # hit ctrl-c
 ps aux | grep /var/lib/rancher/k3s | grep -v grep | awk '{print $2}' | sudo xargs kill
 sudo /usr/local/bin/k3s-killall.sh
