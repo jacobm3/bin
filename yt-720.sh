42f1828f98ab446fb90e4849fa52d445f8261fac
Jacob Martinson <jacobm3@gmail.com>
2023-03-18 00:10:42 -0500

    Change yt-720 from webm to mp4.

--- yt-720.sh
+++ yt-720.sh
@@ -7,8 +7,6 @@ else
   PLID="$1"; shift
 fi
 
-yt-dlp -f "bestvideo[height<=720]+bestaudio/best[height<=720]" \
- --yes-playlist \
- --embed-chapters \
- -o "%(channel)s/%(playlist_title)s-%(playlist_id)s/%(playlist_index)s-%(title)s-%(id)s.%(ext)s" \
+yt-dlp -f "mp4[height<=720]+bestaudio/best[height<=720]" \
+ -o "%(channel)s/%(title)s-%(id)s.%(ext)s" \
   $PLID
