49b376576a98c7a931f0c4a25f51bf13e07397cf
Jacob <jacobm3@gmail.com>
2023-05-17 00:40:01 -0500

    Update cf-dns.ps1

--- windows/cf-dns.ps1
+++ windows/cf-dns.ps1
@@ -1,13 +1,15 @@
 # apply CloudFlare DNS porn & malware blocking to every interface
+# https://blog.cloudflare.com/introducing-1-1-1-1-for-families/
 
 $interfaces = Get-NetAdapter
 
 foreach ($interface in $interfaces) {
     $interfaceName = $interface.Name
-
-    #Write-Host "Setting DNS servers for interface: $interfaceName"
     
+    # just block malware
+    #Set-DnsClientServerAddress -InterfaceAlias $interfaceName -ServerAddresses ("1.1.1.2","1.0.0.2","2606:4700:4700::1112","2606:4700:4700::1002")
+    
+    # block porn and malware
     Set-DnsClientServerAddress -InterfaceAlias $interfaceName -ServerAddresses ("1.1.1.3","1.0.0.3","2606:4700:4700::1113","2606:4700:4700::1003")
-}
 
-#Write-Host "DNS servers set to Cloudflare's servers (1.1.1.3,1.0.0.3,2606:4700:4700::1113,2606:4700:4700::1003) for all network interfaces."
+}
