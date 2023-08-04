ebdac1f22dcef21cd166400dc05e712116452e60
Jacob Martinson <jacobm3@gmail.com>
2023-08-04 14:57:49 -0500

    Add connectivity check before changing DNS servers.

--- windows/cf-dns.ps1
+++ windows/cf-dns.ps1
@@ -1,21 +1,51 @@
-# apply CloudFlare DNS porn & malware blocking to every interface
+# Apply CloudFlare DNS malware and adult content blocking to every interface
 # https://blog.cloudflare.com/introducing-1-1-1-1-for-families/
 
-# Test if DNS server "1.1.1.3" is reachable
-$dnsServer = "1.1.1.3"
-if (Test-Connection -ComputerName $dnsServer -Count 2 -Quiet) {
-    $interfaces = Get-NetAdapter
+$interfaces = Get-NetAdapter
 
-    foreach ($interface in $interfaces) {
-        $interfaceName = $interface.Name
-        
-        # just block malware
-        #Set-DnsClientServerAddress -InterfaceAlias $interfaceName -ServerAddresses ("1.1.1.2","1.0.0.2","2606:4700:4700::1112","2606:4700:4700::1002")
-        
-        # block porn and malware
-        Set-DnsClientServerAddress -InterfaceAlias $interfaceName -ServerAddresses ("1.1.1.3","1.0.0.3","2606:4700:4700::1113","2606:4700:4700::1003")
+# Cloudflare DNS servers, block malware
+$dnsServers = @("1.1.1.2","1.0.0.2","2606:4700:4700::1112","2606:4700:4700::1002")
+
+# Cloudflare DNS servers, block malware and adult content
+$dnsServers = @("1.1.1.3", "1.0.0.3", "2606:4700:4700::1113", "2606:4700:4700::1003")
+
+# Function to test DNS server by sending a DNS query request
+function Test-DnsServerQuery {
+    param (
+        [string[]]$dnsServers
+    )
+
+    $testDomain = "google.com"
+
+    foreach ($server in $dnsServers) {
+        try {
+            $result = Resolve-DnsName -Name $testDomain -Server $server -ErrorAction Stop
+            if (-not $result) {
+                Write-Host "DNS server $server did not respond to the query for $testDomain."
+                return $false
+            }
+        } catch {
+            Write-Host "Failed to query $testDomain on DNS server $server."
+            return $false
+        }
     }
-} else {
-    Write-Host "DNS server $dnsServer is not reachable. Skipping DNS settings."
+
+    return $true
 }
 
+# Test DNS server query
+$areDnsServersResponsive = Test-DnsServerQuery -dnsServers $dnsServers
+
+if ($areDnsServersResponsive) {
+    foreach ($interface in $interfaces) {
+        $interfaceName = $interface.Name
+
+        Write-Host "Setting DNS servers for interface: $interfaceName"
+    
+        Set-DnsClientServerAddress -InterfaceAlias $interfaceName -ServerAddresses $dnsServers
+    }
+
+    Write-Host "DNS servers set to Cloudflare's servers ($dnsServers -join ',') for all network interfaces."
+} else {
+    Write-Host "One or more of the DNS servers did not respond to the DNS query. DNS settings not changed."
+}
