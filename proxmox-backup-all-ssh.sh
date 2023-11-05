57c71845d2f3f3d1e49992c30062eddaa84fe86b
Jacob <jacobm3@gmail.com>
2023-11-05 10:36:01 -0600

    Update proxmox-backup-all-ssh.sh

--- proxmox-backup-all-ssh.sh
+++ proxmox-backup-all-ssh.sh
@@ -2,8 +2,15 @@
 
 # Run vzdump over ssh to backup all VM guests on a remote proxmox server
 
+# Check if PROXMOX_HOST is provided as a command line argument
+if [ -z "$1" ]; then
+  echo "Error: PROXMOX_HOST not provided."
+  echo "Usage: $0 PROXMOX_HOST"
+  exit 1
+fi
+
 # Server details
-PROXMOX_HOST="10.0.0.147"
+PROXMOX_HOST=$1
 USERNAME="root"
 
 tmpfile=/tmp/$$.asdf
@@ -16,7 +23,7 @@ cat $tmpfile | while read -r LINE; do
   VM_ID=$(echo $LINE | awk '{print $1}')
   VM_NAME=$(echo $LINE | awk '{print $2}')
   echo VM_ID=$VM_ID VM_NAME=$VM_NAME
-  ssh ${USERNAME}@${PROXMOX_HOST} vzdump $VM_ID --mode snapshot --compress zstd --stdout </dev/null > vzdump_${VM_ID}_${VM_NAME}_backup.vma.zst
+  ssh ${USERNAME}@${PROXMOX_HOST} vzdump $VM_ID --mode snapshot --compress zstd --stdout </dev/null > vzdump_${VM_ID}_${VM_NAME}_backup.$(date +%Y.%m.%d-%H.%M.%S).vma.zst
 
 done
 
