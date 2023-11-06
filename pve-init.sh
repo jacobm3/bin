9a6021adf4b296005f46e35fa0f140fd32086d11
Jacob <jacobm3@gmail.com>
2023-11-05 18:14:42 -0600

    Update pve-init.sh

--- pve-init.sh
+++ pve-init.sh
@@ -6,7 +6,6 @@ cat > /root/.ssh/authorized_keys <<EOF
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID10aN8gGb0s+3LTE43VNFmvQxz5WYL+JlMCVzmZl+f7 jacob.martinson.ed25519.2022.09
 EOF
 
-sed -Ezi.bak "s/(Ext.Msg.show\(\{\s+title: gettext\('No valid sub)/void\(\{ \/\/\1/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js && systemctl restart pveproxy.service
 
 rm /etc/apt/sources.list.d/*
 echo "deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription" >> /etc/apt/sources.list
@@ -29,3 +28,5 @@ iface vmbr1 inet static
 
 EOF
 
+sed -Ezi.bak "s/(Ext.Msg.show\(\{\s+title: gettext\('No valid sub)/void\(\{ \/\/\1/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js 
+systemctl restart pveproxy.service
