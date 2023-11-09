f6c6c6022025ba4a2b89c5b04602075eb0ae7174
Jacob <jacobm3@gmail.com>
2023-11-09 15:22:18 -0600

    Update observium.sh

--- observium.sh
+++ observium.sh
@@ -8,7 +8,7 @@ sudo ./observium_installscript.sh
 
 
 # setup snmpd 
-sudo apt install -y snmpd
+sudo apt install -y snmpd snmp
 sudo cp /etc/snmp/snmpd.conf /etc/snmp/snmpd.conf.dist
 sudo sed -i 's/^sysLocation .*/sysLocation Houston Office/' /etc/snmp/snmpd.conf
 sudo sed -i 's/^agentaddress .*/agentaddress 0.0.0.0/' /etc/snmp/snmpd.conf
