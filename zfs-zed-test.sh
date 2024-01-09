6057d2f7177e857f0553d3c01175e0ff66c99b1d
Jacob <jacobm3@gmail.com>
2024-01-08 21:31:24 -0600

    Create zfs-zed-test.sh

---
+++ zfs-zed-test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo Your condensed ZFS ZED config:
+cat /etc/zfs/zed.d/zed.rc | grep -v '^\s*$' | grep -v '^#'
+echo
+echo Generating test notification
+set -x
+cd /tmp
+dd if=/dev/zero of=sparse_file bs=1 count=0 seek=100M
+zpool create test /tmp/sparse_file
+zpool scrub test
+zpool export test
+rm sparse_file
