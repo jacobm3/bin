bef267680f47d98fb032c15c41e3ffe233c52e75
Jacob Martinson <jacobm3@gmail.com>
2024-11-15 17:38:44 -0600

    update

---
+++ linux-workstation/ubuntu-server-setup.sh
@@ -0,0 +1,25 @@
+
+apt-get update
+apt-get install \
+7zip \
+apt-transport-https \
+btop \
+curl \
+fio \
+htop \
+jq \
+lm-sensors \
+lshw \
+lsof \
+ncdu \
+nmap \
+pciutils \
+pwgen \
+rclone \
+restic \
+unzip \
+vim \
+wget \
+zip \
+zstd \
+
