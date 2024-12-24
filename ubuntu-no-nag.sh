efbab9773b89817057f9e9fd33603018fd21171a
Jacob <jacobm3@gmail.com>
2024-12-24 12:11:09 -0600

    update

--- ubuntu-no-nag.sh
+++ ubuntu-no-nag.sh
@@ -5,5 +5,7 @@ sudo sed -Ezi.orig \
   -e 's/(def _output_esm_package_alert.*?\n.*?\n.:\n)/\1    return\n/' \
   /usr/lib/update-notifier/apt_check.py
 
+cd /etc/update-motd.d && sudo mkdir -p bak &&  sudo mv -f 10-help-text 50-landscape-sysinfo 50-motd-news bak 
+ 
 sudo /usr/lib/update-notifier/update-motd-updates-available --force
 
