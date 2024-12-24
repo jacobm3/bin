5626579981fb92a909c0ecb1e6abcf716c5e25bf
Jacob <jacobm3@gmail.com>
2024-12-24 12:13:15 -0600

    update

--- ubuntu-no-nag.sh
+++ ubuntu-no-nag.sh
@@ -5,7 +5,13 @@ sudo sed -Ezi.orig \
   -e 's/(def _output_esm_package_alert.*?\n.*?\n.:\n)/\1    return\n/' \
   /usr/lib/update-notifier/apt_check.py
 
-cd /etc/update-motd.d && sudo mkdir -p bak &&  sudo mv -f 10-help-text 50-landscape-sysinfo 50-motd-news bak 
- 
+set -e
+
+cd /etc/update-motd.d 
+sudo mkdir -p bak 
+for file in 10-help-text 50-landscape-sysinfo 50-motd-news; do
+       if [ -f $file ]; then sudo mv $file bak; fi
+       done
+
 sudo /usr/lib/update-notifier/update-motd-updates-available --force
 
