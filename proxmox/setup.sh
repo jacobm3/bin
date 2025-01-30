32f16ad812b7aa2ad8938a51b36f15090f8ab8cf
Jacob <jacobm3@gmail.com>
2025-01-30 13:05:45 -0600

    Update setup.sh

--- proxmox/setup.sh
+++ proxmox/setup.sh
@@ -4,8 +4,14 @@ set -e
 
 mkdir ~/src
 cd ~/src
-apt install nala git
+apt-get update
+apt install nala 
+
+nala upgrade
+
+nala install git
 git clone https://github.com/Meliox/PVE-mods.git
 cd PVE-mods
 ./pve-mod-all.sh
 
+echo Log out of proxmox and clear your cache.
